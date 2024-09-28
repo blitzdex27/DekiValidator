88e8678e0463c64202e13f46376ffcf18a29a694
dexter ramos <blitzdex27@gmail.com>
2024-09-29 03:16:03 +0800

    complete

--- Sources/DekiValidator/DekiValidator.swift
+++ Sources/DekiValidator/DekiValidator.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+struct DekiValidator {
+    
+    
+    private static let regex: String = {
+        "^(?:(?:(?:(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-])+(?:(?:\\.(?!\\.))?(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-])+)*)|(?:\"(?:[^\"\\\\]|\\\\.)*\"))@(?:(?:[a-zA-Z0-9])+(?:\\.(?:[a-zA-Z0-9])+(?:[a-zA-Z0-9-])*(?:[a-zA-Z0-9]))+))$"
+    }()
+    
+    static func validateEmail(_ email: String) -> Bool {
+        let pred = NSPredicate(format: "SELF MATCHES %@", Self.regex)
+        return pred.evaluate(with: email)
+    }
+}
