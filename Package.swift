54553021c1a98957c0df25f9abed6ea9a5194b9b
dexter ramos <blitzdex27@gmail.com>
2024-09-29 03:26:41 +0800

    add tests

--- Package.swift
+++ Package.swift
@@ -12,14 +12,19 @@ let package = Package(
             name: "DekiValidator",
             targets: ["DekiValidator"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/blitzdex27/DekiParser.git", "1.0.0"..<"2.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DekiValidator"),
+            name: "DekiValidator",
+            dependencies: ["DekiParser"]),
         .testTarget(
             name: "DekiValidatorTests",
-            dependencies: ["DekiValidator"]
+            dependencies: ["DekiValidator", "DekiParser"],
+            resources: [.process("Resources")]
         ),
     ]
 )
